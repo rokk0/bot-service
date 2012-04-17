f8d3735de85ff682b08b3318f992e391a446897c
Konstantin Bukley <madundead@gmail.com>
2012-04-17 17:23:15 +0300

    sinatra base added

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source :gemcutter
+source 'http://gems.github.com'
+
+gem 'sinatra'
---
+++ web/server.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require 'sinatra'
+
+# Settings
+# TODO: adjust this settings for our needs
+# TODO: user rabl templates for json rendering
+#set :haml, :format => :html5
+#set :haml, :layout => :'layouts/default'
+#set :views, File.dirname(__FILE__) + '/views'
+#set :public_folder, File.dirname(__FILE__) + '/assets'
+
+# Routes
+get '/' do
+end
